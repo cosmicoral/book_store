a52c4ba3ab86d061fbf0077013dc0c52c48234c4
Yu Han <coralhanyu@outlook.com>
2026-05-20 03:44:24 +0100

    fix database connection usage

--- lib/book_repository.py
+++ lib/book_repository.py
@@ -5,19 +5,58 @@ class BookRepository:
         self._connection = connection
 
     def all(self):
-        cursor = self._connection.cursor()
-        cursor.execute("SELECT * FROM books")
+        self._connection.connect()
 
-        rows = cursor.fetchall()
-    
+        rows = self._connection.connection.execute(
+            "SELECT * FROM books"
+        ).fetchall()
+
         books = []
+
         for row in rows:
-            books.append(Book(row["id"], row["title"], row["author_name"]))
-        
+            books.append(
+                Book(
+                    row["id"],
+                    row["title"],
+                    row["author_name"]
+                )
+            )
+
         return books
-    
+
     def create(self, book):
-        cursor = self._connection.cursor()
-        cursor.execute(
-        "INSERT INTO books(title, author_name) VALUES (%s, %s)",
-        [book.title, book.author_name])
+        self._connection.connect()
+
+        self._connection.connection.execute(
+            "INSERT INTO books(title, author_name) VALUES (%s, %s)",
+            [book.title, book.author_name]
+        )
+
+        self._connection.connection.commit()
+
+
+
+# old one：
+# from lib.book import Book
+
+# class BookRepository:
+#     def __init__(self, connection):
+#         self._connection = connection
+
+#     def all(self):
+#         cursor = self._connection.cursor()
+#         cursor.execute("SELECT * FROM books")
+
+#         rows = cursor.fetchall()
+    
+#         books = []
+#         for row in rows:
+#             books.append(Book(row["id"], row["title"], row["author_name"]))
+        
+#         return books
+    
+#     def create(self, book):
+#         cursor = self._connection.cursor()
+#         cursor.execute(
+#         "INSERT INTO books(title, author_name) VALUES (%s, %s)",
+#         [book.title, book.author_name])
