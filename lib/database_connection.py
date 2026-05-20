2cf4c4310e053c576422b5f3d6b827d79f3008d1
Yu Han <coralhanyu@outlook.com>
2026-05-20 02:19:14 +0100

    add github actions deployment

--- lib/database_connection.py
+++ lib/database_connection.py
@@ -2,65 +2,45 @@ import psycopg
 from psycopg.rows import dict_row
 import os
 
-# This class helps us interact with the database.
-# It wraps the underlying psycopg library that we are using.
-
-# If the below seems too complex right now, that's OK.
-# That's why we have provided it!
 class DatabaseConnection:
+
     DATABASE_NAME = os.getenv(
-    "DATABASE_NAME",
-    "book_store") 
+        "DATABASE_NAME",
+        "book_store"
+    )
+
     DATABASE_HOST = os.getenv(
-    "DATABASE_HOST",
-    "localhost"
-)# <-- CHANGE THIS!
+        "DATABASE_HOST",
+        "localhost"
+    )
+
+    DATABASE_USER = os.getenv(
+        "DATABASE_USER",
+        "yuhan"
+    )
+
+    DATABASE_PASSWORD = os.getenv(
+        "DATABASE_PASSWORD",
+        ""
+    )
 
     def __init__(self):
         self.connection = None
 
-    # This method connects to PostgreSQL using the psycopg library. We connect
-    # to localhost and select the database name given in argument.
     def connect(self):
-        try:
-            self.connection = psycopg.connect(
-                f"postgresql://yuhan@{self.DATABASE_HOST}/{self.DATABASE_NAME}",
-                row_factory=dict_row)
-        except psycopg.OperationalError:
-            raise Exception(f"Couldn't connect to the database {self.DATABASE_NAME}."
-                            f"Did you create it using `createdb {self.DATABASE_NAME}`?")
 
-    # This method seeds the database with the given SQL file.
-    # We use it to set up our database ready for our tests or application.
-    def seed(self, sql_filename):
-        self._check_connection()
-        if not os.path.exists(sql_filename):
-            raise Exception(f"File {sql_filename} does not exist")
-        with self.connection.cursor() as cursor:
-            cursor.execute(open(sql_filename, "r").read())
-            self.connection.commit()
-    
+        connection_string = (
+            f"postgresql://{self.DATABASE_USER}"
+        )
 
-    # This method executes an SQL query on the database.
-    # It allows you to set some parameters too. You'll learn about this later.
-    def execute(self, query, params=[]):
-        self._check_connection()
-        with self.connection.cursor() as cursor:
-            cursor.execute(query, params)
-            if cursor.description is not None:
-                result = cursor.fetchall()
-            else:
-                result = None
-            self.connection.commit()
-            return result
+        if self.DATABASE_PASSWORD != "":
+            connection_string += f":{self.DATABASE_PASSWORD}"
 
-    CONNECTION_MESSAGE = '' \
-        'DatabaseConnection.exec_params: Cannot run a SQL query as ' \
-        'the connection to the database was never opened. Did you ' \
-        'make sure to call first the method DatabaseConnection.connect` ' \
-        'in your app.py file (or in your tests)?'
+        connection_string += (
+            f"@{self.DATABASE_HOST}/{self.DATABASE_NAME}"
+        )
 
-    # This private method checks that we're connected to the database.
-    def _check_connection(self):
-        if self.connection is None:
-            raise Exception(self.CONNECTION_MESSAGE)
+        self.connection = psycopg.connect(
+            connection_string,
+            row_factory=dict_row
+        )
